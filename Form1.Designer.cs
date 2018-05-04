2226fb44a1d5f5d20295819aed3689b3779c227c
Niall Barr <niall@nbsoftware.com>
2018-05-04 11:54:09 +0100

    Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BiblioRenamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteOriginalFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeNameFromListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToDirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(749, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(749, 544);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.importToolStripMenuItem.Text = "&Import RefWorks Tagged";
+            this.importToolStripMenuItem.Click += new System.EventHandler(this.importToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteOriginalFileToolStripMenuItem,
+            this.removeNameFromListToolStripMenuItem,
+            this.copyToDirToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // deleteOriginalFileToolStripMenuItem
+            // 
+            this.deleteOriginalFileToolStripMenuItem.Name = "deleteOriginalFileToolStripMenuItem";
+            this.deleteOriginalFileToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.deleteOriginalFileToolStripMenuItem.Text = "&Delete original file";
+            this.deleteOriginalFileToolStripMenuItem.Click += new System.EventHandler(this.deleteOriginalFileToolStripMenuItem_Click);
+            // 
+            // removeNameFromListToolStripMenuItem
+            // 
+            this.removeNameFromListToolStripMenuItem.Name = "removeNameFromListToolStripMenuItem";
+            this.removeNameFromListToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.removeNameFromListToolStripMenuItem.Text = "&Remove name from list";
+            this.removeNameFromListToolStripMenuItem.Click += new System.EventHandler(this.removeNameFromListToolStripMenuItem_Click);
+            // 
+            // copyToDirToolStripMenuItem
+            // 
+            this.copyToDirToolStripMenuItem.Name = "copyToDirToolStripMenuItem";
+            this.copyToDirToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.copyToDirToolStripMenuItem.Text = "&Copy to dir";
+            this.copyToDirToolStripMenuItem.Click += new System.EventHandler(this.copyToDirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 568);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Bibliography File Renamer ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteOriginalFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeNameFromListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToDirToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿/*
+   Copyright 2018 Niall S F Barr
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace BiblioRenamer
+{
+    public partial class Form1 : Form
+    {
+        private string outDir;
+        private bool deleteOriginal;
+        private bool removeFromList;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            outDir = null;
+            deleteOriginal = false;
+            removeFromList = true;
+            removeNameFromListToolStripMenuItem.Checked = removeFromList;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string filename = null;
+            // From https://www.codeproject.com/Articles/9017/A-Simple-Drag-And-Drop-How-To-Example
+            if ((e.AllowedEffect & DragDropEffects.Copy) == DragDropEffects.Copy)
+            {
+                Array data = ((IDataObject)e.Data).GetData("FileName") as Array;
+                if (data != null)
+                {
+                    if ((data.Length == 1) && (data.GetValue(0) is String))
+                    {
+                        filename = ((string[])data)[0];
+                    }
+                }
+                //MessageBox.Show(filename);
+            }
+            if ((filename != null) && (File.Exists(filename)))
+            {
+                int idx = listBox1.IndexFromPoint(listBox1.PointToClient(new Point(e.X, e.Y)));
+                if (idx >= 0)
+                {
+                    listBox1.SetSelected(idx, true);
+                    string ext = Path.GetExtension(filename).ToLower();
+                    string outPath;
+                    if (outDir != null)
+                        outPath = outDir;
+                    else
+                        outPath = Path.GetDirectoryName(filename);
+                    outPath += Path.DirectorySeparatorChar + listBox1.Items[idx].ToString() + ext;
+                    MessageBox.Show(outPath);
+                    if(File.Exists(outPath))
+                    {
+                        MessageBox.Show(outPath + "\r\nalready exists, so nothing was done.");
+                    }
+                    else
+                    {
+                        File.Copy(filename, outPath);
+                        if (deleteOriginal)
+                            File.Delete(filename);
+                        if (removeFromList)
+                            listBox1.Items.RemoveAt(idx);
+                    }
+                }
+            }
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            //MessageBox.Show(e.Data.ToString());
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;  
+            }
+        }
+
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    importBibliography(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void importBibliography(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            listBox1.Items.Clear();
+            List<string> authors = new List<string>();
+            List<string> filenames = new List<string>();
+            string year = "";
+            string title = "";
+            bool canuse = false;
+            for(int n=0; n<lines.Length; n++)
+            {
+                if(lines[n].Length == 0)
+                {
+                    if(canuse)
+                    {
+                        string fn = "";
+                        if (authors.Count > 0)
+                        {
+                            if (authors.Count == 1)
+                                fn = authors[0] + " - ";
+                            else if (authors.Count == 2)
+                                fn = authors[0] + " & " + authors[1] + " - ";
+                            else
+                                fn = authors[0] + " et al - ";
+                        }
+                        if (year.Length > 0)
+                            fn += year + " - ";
+
+                        if (title.Length > 0)
+                            fn += title + " ";
+                        if (fn.Length > 120)
+                            fn = fn.Substring(0, 120);
+                        filenames.Add(fn);
+
+                    }
+                    canuse = false;
+                    authors.Clear();
+                    year = "";
+                    title = "";
+                }
+                else
+                {
+                    string type = lines[n].Substring(0, 2);
+                    string value = lines[n].Substring(3).Trim();
+                    switch(type)
+                    {
+                        case "T1":
+                            title = Regex.Replace(value, @"[^\w]+", " ");
+                            canuse = true;
+                            break;
+                        case "YR":
+                            year = value;
+                            break;
+                        case "A1":
+                            if(value.IndexOf(",")>0)
+                                authors.Add(value.Substring(0, value.IndexOf(",")));
+                            else
+                                authors.Add(value);
+                            break;
+                    }
+                }
+            }
+            filenames.Sort();
+            foreach (string fn in filenames)
+                listBox1.Items.Add(fn);
+        }
+
+        private void deleteOriginalFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            deleteOriginal = !deleteOriginal;
+            deleteOriginalFileToolStripMenuItem.Checked = deleteOriginal;
+        }
+
+        private void removeNameFromListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            removeFromList = !removeFromList;
+            removeNameFromListToolStripMenuItem.Checked = removeFromList;
+        }
+
+        private void copyToDirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(copyToDirToolStripMenuItem.Checked)
+            {
+                copyToDirToolStripMenuItem.Checked = false;
+                outDir = null;
+                Text = "Bibliography File Renamer";
+            }
+            else
+            {
+                FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+                folderBrowserDialog1.Description = "Select the directory that you want to copy renamed files to.";
+                folderBrowserDialog1.ShowNewFolderButton = true;
+                //folderBrowserDialog1.RootFolder = Environment.SpecialFolder.Personal;
+                if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    outDir = folderBrowserDialog1.SelectedPath;
+                    copyToDirToolStripMenuItem.Checked = true;
+                    Text = "Bibliography File Renamer - target: " + outDir;
+
+                }
+            }
+        }
+    }
+}
